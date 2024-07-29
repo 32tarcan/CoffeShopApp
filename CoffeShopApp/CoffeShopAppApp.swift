6ee4707c4f819421589c771a5ce3c5d4ece833e9
32tarcan <tarcann.bahadir@gmail.com>
2024-07-29 17:49:03 +0300

    Main Menu name changed and adding OnboardingView File.

--- CoffeShopApp/CoffeShopAppApp.swift
+++ CoffeShopApp/CoffeShopAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CoffeShopAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainMenu()
         }
     }
 }
---
+++ CoffeShopApp/Views/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  CoffeShopApp
+//
+//  Created by Sakans on 29.07.2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
