52ea6903b017cb8426161eea6b2e6ad4e0c0c672
32tarcan <tarcann.bahadir@gmail.com>
2024-07-29 17:30:24 +0300

    Initial Commit

---
+++ CoffeShopApp/CoffeShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopAppApp.swift
+//  CoffeShopApp
+//
+//  Created by Sakans on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
