31e79408c4baf2768893b2ce8c497921c7424aed
32tarcan <tarcann.bahadir@gmail.com>
2024-08-01 01:16:16 +0300

    Assets folder updated and MapView file created.

---
+++ CoffeShopApp/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  CoffeShopApp
+//
+//  Created by Sakans on 1.08.2024.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
